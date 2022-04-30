9beb3d166ea30c84d5de565911ede0e158e2b2a6
sinkholenet <104696757+sinkholenet@users.noreply.github.com>
2022-05-01 00:00:35 +0300

    Add files via upload

---
+++ read_data.m
@@ -0,0 +1,17 @@
+Orthomosaic=imread('RGB.png');
+Slope_Org=imread('slope_2.tif');
+Slope_Data=zeros(size(a));
+for i=1:15979
+    for j=1:14770
+        if Slope_Org(i,j)<0
+            Slope_Data(i,j)=0;
+        else
+            Slope_Data(i,j)=Slope_Org(i,j);
+        end
+    end
+end
+imshow(uint8(Orthomosaic),[])
+figure
+clims = [0 15];
+imagesc(Slope_Data,clims)
+colorbar
